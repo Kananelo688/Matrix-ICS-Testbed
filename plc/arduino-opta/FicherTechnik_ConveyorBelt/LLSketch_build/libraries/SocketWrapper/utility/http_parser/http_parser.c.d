d8a406c3cb4725e893adbbf1fd0d7e13fe32b9d5
Kananelo688 <kchabeli688@gmail.com>
2026-07-28 21:18:31 +0200

    migrated plc projects to this repository

---
+++ plc/arduino-opta/FicherTechnik_ConveyorBelt/LLSketch_build/libraries/SocketWrapper/utility/http_parser/http_parser.c.d
@@ -0,0 +1,3 @@
+C:\Users\chabz\Documents\PLC\Arduino\ Opta\FicherTechnik_ConveyorBelt\LLSketch_build\libraries\SocketWrapper\utility\http_parser\http_parser.c.o: \
+ C:\Users\chabz\AppData\Local\T\A\internal\arduino_mbed_opta_3.5.4_ccd8464f766ff0fc\libraries\SocketWrapper\src\utility\http_parser\http_parser.c \
+ C:\Users\chabz\AppData\Local\T\A\internal\arduino_mbed_opta_3.5.4_ccd8464f766ff0fc\libraries\SocketWrapper\src\utility\http_parser\http_parser.h
